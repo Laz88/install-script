a6727141a435ff621463c373d081a143e043820f
Andrew Champagne <33033657+Laz88@users.noreply.github.com>
2017-11-13 14:44:21 -0500

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -14,13 +14,6 @@
 # (5) when the script completes, you should be able to use the software that was installed.
 
 
-# This is how you comment a line in bash
-#
-echo "If you are Andrew and have a backup located at /TB/backup, then type restore"
-echo "This script can backup or restore files."
-echo "		Type restore to restore files."
-echo "		Otherwise type ignore"
-read SITUATION
 
 
 # This is how you set a variable in "BASH"
@@ -71,27 +64,6 @@ cd Youtube-DLG-0.4
 sudo python setup.py install
 cd ..
 
-# Now we're creating launchable icons
-sudo cat > /usr/share/applications/youtube-dl-gui.desktop << EOL
-[Desktop Entry]
-Name=Youtube-DL-GUI
-Exec=youtube-dl-gui
-Terminal=false
-Type=Application
-Icon=transmission
-Categories=GTK;GNOME;Utility;
-EOL
-
-sudo cat > /usr/share/applications/web-wechat.desktop << EOL
-[Desktop Entry]
-Name=WeChat
-Exec=chromium-browser https://web.wechat.com/
-Terminal=false
-Type=Application
-Icon=/usr/share/ibus-table/icons/chinese.svg
-Categories=GTK;GNOME;Utility;
-EOL
-
 # Previously we ran the command "mkdir app-downloads" and downloaded files to it. Lets delete the folder and files. 
 cd ..
 sudo rm -R app-downloads
@@ -114,13 +86,14 @@ sudo chmod 777 virtualbox-5.2_5.2.0-118431~Ubuntu~zesty_amd64.deb
 
 echo "${scriptuser} is now installing MORE great software"
 sudo apt-get install ffmpeg -y
-sudo apt-get install inkscape git gimp p7zip-full livegnome-session gnome-maps gnome-weather polari gnome-documents gnome-photos gnome-musicstreamer geany deluge chromium-browser mumble krita obs-studio gnome-tweak-tool ibus-sunpinyin ibus-pinyin steam -y
+sudo apt-get install inkscape git gimp p7zip-full geany deluge chromium-browser mumble krita obs-studio gnome-tweak-tool ibus-sunpinyin ibus-pinyin steam -y
 sudo snap install atom --classic 
 ibus restart
 # The above also installed some Chinese language specific files called IBUS-pinyin, IBUS-sunpinyin, and then it told IBUS to restart.
 # This did not activate the Chinese language, to do that you'll have to follow this youtube video instruction:
 
 # The below commands are good to run after you install or uninstall large batches of programs. They clean up and maintain the system.
+sudo apt-get --fix-broken install
 sudo apt-get autoremove
 sudo apt-get autoclean
 
@@ -131,26 +104,32 @@ sudo apt-get autoclean
 
 cd /home/"${scriptuser}"/.steam/
 wget "https://github.com/Laz88/install-script/raw/master/skins.zip"
-7zip x skins.zip
+7z x skins.zip
 
-echo "${scriptuser} is now getting a great wallpaper."
+
+
+# Gnome related
+sudo apt-get install chrome-gnome-shell gnome-session -y
+sudo apt-get install gnome-maps gnome-weather polari gnome-documents gnome-photos gnome-music -y
+sudo apt-get --fix-broken install
+sudo apt-get autoremove
+sudo apt-get autoclean
+# Theme related
 cd /home/"${scriptuser}"/Pictures/
 wget "https://raw.githubusercontent.com/Laz88/install-script/master/mbuntu-0.jpg"
 wget "https://raw.githubusercontent.com/Laz88/install-script/master/00img102.jpg"
 gsettings set org.gnome.desktop.background picture-uri "/home/${scriptuser}/Pictures/mbuntu-0.jpg"
 
-echo "${scriptuser} is now done! Install some extensions for GNOME (the system UI) now..."
-sudo apt-get install chrome-gnome-shell gnome-session gnome-session gnome-maps gnome-weather polari gnome-documents gnome-photos gnome-music -y
-
-
-cd /home/${scriptuser}/
+cd /home/${scriptuser}/Downloads
 wget "https://dl.opendesktop.org/api/files/download/id/1498225522/ocs-url_3.0.2-0ubuntu1_amd64.deb"
 sudo apt install libqt5svg5 qml-module-qtquick-controls
-sudo dpkg -i /home/${scriptuser}/ocs-url*.deb
+sudo dpkg -i /home/${scriptuser}/Downloads/ocs-url*.deb
+sudo apt-get --fix-broken install
+sudo apt-get autoremove
+sudo apt-get autoclean
 
-chomium-browser https://extensions.gnome.org/extension/19/user-themes/ https://extensions.gnome.org/extension/906/sound-output-device-chooser/ https://extensions.gnome.org/extension/549/web-search-dialog/
-
-mkrdir /home/"${scriptuser}"/scripts
+# Make Scripts
+mkdir /home/"${scriptuser}"/scripts
 sudo cat > /home/"${scriptuser}"/scripts/144hz-displayport-2.sh << EOL
 #!/bin/bash
 xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144.00
@@ -183,7 +162,7 @@ fi
 if [ "$SITUATION" == restore ]; then
 	# cp -r /media/$USER/TB/backup/.config /home/$USER
 	cp -r /media/$USER/TB/backup/.thunderbird /home/$USER
-	cp -r /media/$USER/TB/backup.mozilla /home/$USER
+	# cp -r /media/$USER/TB/backup.mozilla /home/$USER
 	cp -r /media/$USER/TB/backup/.steam /home/$USER
 	cp -r /media/$USER/TB/backup/Documents /home/$USER
 	cp -r /media/$USER/TB/backup/Downloads /home/$USER
@@ -198,13 +177,37 @@ sudo chmod +x backup.sh
 sudo chmod 777 144hz-displayport-2.sh
 sudo chmod +x 144hz-displayport-2.sh
 
+# Make Icons
+sudo cat > /usr/share/applications/youtube-dl-gui.desktop << EOL
+[Desktop Entry]
+Name=Youtube-DL-GUI
+Exec=youtube-dl-gui
+Terminal=false
+Type=Application
+Icon=transmission
+Categories=GTK;GNOME;Utility;
+EOL
+
+sudo cat > /usr/share/applications/web-wechat.desktop << EOL
+[Desktop Entry]
+Name=WeChat
+Exec=chromium-browser https://web.wechat.com/
+Terminal=false
+Type=Application
+Icon=/usr/share/ibus-table/icons/chinese.svg
+Categories=GTK;GNOME;Utility;
+EOL
+
+SITUATION=restore
 if [ "$SITUATION" == restore ]; then
 	# cp -r /media/$USER/TB/backup/.config /home/$USER
 	cp -r /media/$USER/TB/backup/.thunderbird /home/$USER
-	cp -r /media/$USER/TB/backup.mozilla /home/$USER
+	# cp -r /media/$USER/TB/backup/.mozilla /home/$USER
 	cp -r /media/$USER/TB/backup/.steam /home/$USER
 	cp -r /media/$USER/TB/backup/Documents /home/$USER
 	cp -r /media/$USER/TB/backup/Downloads /home/$USER
 else
 	echo "$USER did not specify restore."
 fi
+
+chromium-browser  https://extensions.gnome.org/extension/19/user-themes/ https://extensions.gnome.org/extension/906/sound-output-device-chooser/ https://extensions.gnome.org/extension/549/web-search-dialog/
